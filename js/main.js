0d5ccc0062cef74ffc315ee19414a303d3d59a13
Victor Samoylik <v.samojlik@gmail.com>
2022-01-13 18:32:20 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// $(document).ready(function(){
+// 	$('.degree__slider').slick({
+// 		adaptiveHeight: true,
+// 		// autoplay: true,
+// 		// autoplaySpeed: 2500,		
+// 		arrows: true,
+// 		prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/arrow-left.svg" alt=""></button>',
+// 		nextArrow: '<button type="button" class="slick-next"><img src="img/icons/arrow-right.svg" alt=""></button>',
+// 	 });
+// });
